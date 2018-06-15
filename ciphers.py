b273e770f1b0c0b8dd6f15a820636c02828780da
Kahlia Lepple <klepple@uwo.ca>
2018-06-15 16:09:03 -0400

    add encrypt/decrypt for simple sub

--- ciphers.py
+++ ciphers.py
@@ -43,11 +43,39 @@ class CaesarCipher:
 class SimpleSubstitutionCipher:
     def __init__(self, key='abc'):
         self.key = key
+        self.cipher_alphabet = self.generate_cipher_alphabet()
 
     def generate_cipher_alphabet(self):
+        """Generates a simple sub cipher alphabet."""
         cipher_alphabet = []
         for i in range(len(self.key)):
             char = self.key[i]
             cipher_index = PLAIN_ALPHABET.index(char)
-            cipher_alphabet.__add__(cipher_index)
+            cipher_alphabet.insert(cipher_index)
         return cipher_alphabet
+
+    def encrypt(self, plain_text):
+        """Encrypts the plaintext using a simple sub cipher."""
+        cipher_text = StringIO()
+        for i in range(len(plain_text)):
+            char = plain_text[i]
+            if char == ' ':
+                cipher_text.write(' ')
+            else:
+                alphabet_index = PLAIN_ALPHABET.index(char)
+                simple_sub_char = self.cipher_alphabet.__getitem__(alphabet_index)
+                cipher_text.write(simple_sub_char)
+        return cipher_text
+
+    def decrypt(self, cipher_text):
+        """Decrypts the ciphertext that has been encrypted using a simple sub cipher."""
+        plain_text = StringIO()
+        for i in range(len(cipher_text)):
+            char = cipher_text[i]
+            if char == ' ':
+                plain_text.write(' ')
+            else:
+                alphabet_index = PLAIN_ALPHABET.index(char)
+                simple_sub_char = self.cipher_alphabet.__getitem__(alphabet_index)
+                plain_text.write(simple_sub_char)
+        return plain_text
